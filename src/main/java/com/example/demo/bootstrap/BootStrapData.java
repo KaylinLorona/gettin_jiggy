7a6638f663f7ad11569a1e45c54947e59eadc4a6
Kaylin Lorona <kaylinlorona10@gmail.com>
2024-05-21 20:40:29 -0500

    Added 5th part as well as 5 products and sample inventory for item E

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -41,42 +41,69 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        //Creating object from the Part class
-        InhousePart guitarPick = new InhousePart();
-        InhousePart guitarString = new InhousePart();
-        InhousePart ampCord = new InhousePart();
-        InhousePart guitarBridge = new InhousePart();
+        if (partRepository.count() == 0) {
 
-        //Setting the values for guitarPick
-        guitarPick.setName("Guitar Pick");
-        guitarPick.setPrice(2.99);
-        guitarPick.setInv(75);
-        //Saving in the repository
-        partRepository.save(guitarPick);
+            //Creating object from the Part class
+            InhousePart guitarPick = new InhousePart();
+            InhousePart guitarString = new InhousePart();
+            InhousePart ampCord = new InhousePart();
+            InhousePart guitarBridge = new InhousePart();
+            InhousePart guitarStrap = new InhousePart();
 
-        //Setting the values for guitarString
-        guitarString.setName("Guitar String (4 strings mix & match)");
-        guitarString.setPrice(15.99);
-        guitarString.setInv(100);
-        //Saving in the repository
-        partRepository.save(guitarString);
+            //Setting the values for guitarPick
+            guitarPick.setName("Guitar Pick");
+            guitarPick.setPrice(2.99);
+            guitarPick.setInv(75);
+            //Saving in the repository
+            partRepository.save(guitarPick);
 
-        //Setting the values for ampCord
-        ampCord.setName("Amp Cord");
-        ampCord.setPrice(10.00);
-        ampCord.setInv(15);
-        //Saving in the repository
-        partRepository.save(ampCord);
+            //Setting the values for guitarString
+            guitarString.setName("Guitar String (4 strings mix & match)");
+            guitarString.setPrice(15.99);
+            guitarString.setInv(100);
+            //Saving in the repository
+            partRepository.save(guitarString);
 
-        //Setting the values for guitarBridge
-        guitarBridge.setName("Guitar Bridge");
-        guitarBridge.setPrice(9.50);
-        guitarBridge.setInv(20);
-        //Saving in the repository
-        partRepository.save(guitarBridge);
+            //Setting the values for ampCord
+            ampCord.setName("Amp Cord");
+            ampCord.setPrice(10.00);
+            ampCord.setInv(15);
+            //Saving in the repository
+            partRepository.save(ampCord);
 
+            //Setting the values for guitarBridge
+            guitarBridge.setName("Guitar Bridge");
+            guitarBridge.setPrice(9.50);
+            guitarBridge.setInv(20);
+            //Saving in the repository
+            partRepository.save(guitarBridge);
 
+            //Setting the values for guitarStrap
+            guitarStrap.setName("Guitar Strap");
+            guitarStrap.setPrice(10.50);
+            guitarStrap.setInv(40);
+            //Saving in the repository
+            partRepository.save(guitarStrap);
 
+        }
+
+        if (productRepository.count() == 0) {
+
+            //Creating object form the Product Class
+            Product acousticGuitar = new Product("Acoustic Guitar",129.99,4);
+            Product electricGuitar = new Product("Electric Guitar", 299.99, 2);
+            Product fourStringBass = new Product("Four String Bass", 179.99, 5);
+            Product electricGuitarAmp = new Product("Electric Guitar Amp", 99.99, 5);
+            Product electricBassAmp = new Product("Electric Bass Amp", 99.99, 3);
+
+            //Saving in the repository
+            productRepository.save(acousticGuitar);
+            productRepository.save(electricGuitar);
+            productRepository.save(fourStringBass);
+            productRepository.save(electricGuitarAmp);
+            productRepository.save(electricBassAmp);
+
+        }
        /*
         OutsourcedPart o= new OutsourcedPart();
         o.setCompanyName("Western Governors University");
