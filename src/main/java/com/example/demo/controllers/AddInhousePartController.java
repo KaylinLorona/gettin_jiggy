ed324fe2594d54f16640e0b03599c27c5fb4cc4d
Kaylin Lorona <kaylinlorona10@gmail.com>
2024-06-03 22:08:29 -0500

    no changes made to page

--- src/main/java/com/example/demo/controllers/AddInhousePartController.java
+++ src/main/java/com/example/demo/controllers/AddInhousePartController.java
@@ -11,10 +11,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
